149f5bbd465ae2d336a2528d03915824e583bd1c
Kunkun <jikunyang03@gmail.com>
2025-02-03 11:27:04 +1300

    added a page transition js

--- webflow/main.js
+++ webflow/main.js
@@ -8,6 +8,8 @@ import contactTabClick from './button-functions/contactTabClick'
 import formTab from './button-functions/formTab'
 import backBtnClick from './button-functions/backButtonClick'
 import articleClick from './button-functions/articleClick'
+import pageTransition from './button-functions/pageTransition'
+
 
 export default function initWebflowFunctions() {
     preloaderAnime()
@@ -19,4 +21,5 @@ export default function initWebflowFunctions() {
     textIn()
     burgerMenuOpen()
     backBtnClick()
+    pageTransition()
 }
